afc02a2fb00227a8bf836facdf2f2a19969e3715
Caleb Stone <caleblstone@gmail.com>
2020-07-14 19:11:01 -0400

    Added hover colors

--- hand.js
+++ hand.js
@@ -10,11 +10,12 @@ let rect = line.getBoundingClientRect()
 let rectHeight = rect.height
 
 let title = document.querySelector('header section.title')
+let subtitle = document.querySelector('header section.subtitle')
 let rect2 = title.getBoundingClientRect()
 let rightSide = rect2.right
 
-let socialMedia = document.querySelectorAll('section.socialmedia')
-
+let links = document.querySelectorAll('a')
+let underline = document.querySelectorAll('u')
 
 function setup() {
   createCanvas(width, height);
@@ -25,42 +26,70 @@ function setup() {
 function draw() {
   clear();
   strokeWeight(10);
-point(rightSide + 32, rectHeight/2);
-//point(width/3*2, 20);
-//point(mouseX, mouseY);
+  point(rightSide + 32, rectHeight / 2);
+  //point(width/3*2, 20);
+  //point(mouseX, mouseY);
 
-noFill();
-strokeWeight(4);
-beginShape();
-vertex(rightSide + 32, rectHeight/2);
-quadraticVertex(width/3*2, rectHeight/2, mouseX, mouseY);
-endShape();
+  noFill();
+  strokeWeight(4);
+  beginShape();
+  vertex(rightSide + 32, rectHeight / 2);
+  quadraticVertex(width / 3 * 2, rectHeight / 2, mouseX, mouseY);
+  endShape();
 
-let diffX = (width/3*2)-mouseX;
-let diffY = mouseY - 20;
+  let diffX = (width / 3 * 2) - mouseX;
+  let diffY = mouseY - 20;
 
-angle = Math.atan2(diffY, diffX)
-
-
-push()
-angleMode(RADIANS)
-translate(mouseX,mouseY)
-rotate(-angle + radians(90));
-image(hand,-30, -30);
-pop()
+  angle = Math.atan2(diffY, diffX)
 
 
+
+
+
+  colorChange()
 }
 
-// socailMedia.forEach(element => {
-//   element.addEventListner("mouseenter", function(){
-//
-//   })
-// })
-
 socialMedia.forEach(tag => {
-  socialMedia.addEventListener("click", function() {
+  this.addEventListener("mouseMoved()", function() {
     tag.style.color = "#35AC7D"
     console.log("hello");
   })
 })
+
+function pushPop() {
+  push()
+  angleMode(RADIANS)
+  translate(mouseX, mouseY)
+  rotate(-angle + radians(90));
+  image(hand, -30, -30);
+  pop()
+}
+
+
+
+function colorChange() {
+  links.forEach(tag => {
+    let rect = tag.getBoundingClientRect()
+    if (mouseX > rect.left && mouseX < rect.right && mouseY < rect.bottom && mouseY > rect.top) {
+      tag.style.color = "#E7511C"
+      tag.style.textDecorationColor = "#E7511C"
+      image(hand, rect.left - 40, rect.top - 100);
+    } else {
+      pushPop()
+      tag.style.color = "black"
+    }
+  })
+  underline.forEach(tag => {
+    let rect = tag.getBoundingClientRect()
+    if (mouseX > rect.left && mouseX < rect.right && mouseY < rect.bottom && mouseY > rect.top) {
+      tag.style.color = "#35AC7D"
+      tag.style.textDecorationColor = "#35AC7D"
+
+    } else {
+      pushPop()
+      tag.style.color = "black"
+    }
+  })
+
+
+}
